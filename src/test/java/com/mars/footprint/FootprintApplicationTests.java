2397dd1cdde4b382fc890800806a4c53ce58cdfa
ting wang <ting@guiker.com>
2023-07-30 23:32:13 -0400

    project setup

---
+++ src/test/java/com/mars/footprint/FootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.footprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
